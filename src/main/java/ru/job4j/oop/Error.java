4505ef6952483c0206c3546adfbe9e570b932826
Corsair <corsair954@mail.ru>
2022-03-28 20:25:04 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,32 @@
+package ru.job4j.oop;
+
+public class Error {
+    boolean active;
+    int status;
+    String message;
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public Error() {
+
+    }
+
+    public void  printInfo() {
+        System.out.println(active);
+        System.out.println(status);
+        System.out.println(message);
+    }
+
+    public static void main(String[] args) {
+        Error error1 = new Error();
+        Error error2 = new Error(true, 234, "CPU usage is high");
+        Error error3 = new Error(false, 12, "None");
+        error1.printInfo();
+        error2.printInfo();
+        error3.printInfo();
+    }
+}
