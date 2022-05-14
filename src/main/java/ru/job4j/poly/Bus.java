729fc87d90063297960fdbb96d206a750451de31
Corsair <corsair954@mail.ru>
2022-05-14 14:01:28 +0300

    3. Интерфейс Input [#181092 #295156]

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -3,16 +3,16 @@ package ru.job4j.poly;
 public class Bus implements Transport {
     @Override
     public void going() {
-
+        System.out.println("Bus go");
     }
 
     @Override
     public void passengers(int amount) {
-
+        System.out.println(amount);
     }
 
     @Override
     public float refuel(float amount) {
-        return 0;
+        return amount;
     }
 }
