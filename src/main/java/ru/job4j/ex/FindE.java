dc6275ac0447d7a6a02bd3cb4d595c351bbc9bda
Corsair <corsair954@mail.ru>
2022-06-08 15:02:26 +0300

    0.4. Пользовательские исключения. [#219367]

---
+++ src/main/java/ru/job4j/ex/FindE.java
@@ -0,0 +1,28 @@
+package ru.job4j.ex;
+
+public class FindE {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (value[i].equals(key)) {
+                rsl = i;
+                break;
+            }
+        }
+        if (rsl == -1) {
+            throw new ElementNotFoundException("Element is not found");
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        String[] exemple = {
+                "Flower", "House", "Cup", "Bicycle"
+        };
+        try {
+            FindE.indexOf(exemple, "Cu");
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
