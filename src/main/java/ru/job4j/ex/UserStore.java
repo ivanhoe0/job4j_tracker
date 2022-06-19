1b1a10623b997bec4f7f372878f1cf3fa0bc8893
Corsair <corsair954@mail.ru>
2022-06-19 22:18:34 +0300

    0.5. Иерархия исключений и множественный catch. [#219368 #312748]

--- src/main/java/ru/job4j/ex/UserStore.java
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -20,7 +20,7 @@ public class UserStore {
         if (!user.isValid() || user.getUsername().length() < 3) {
             throw new UserInvalidException("User is invalid");
         }
-        return user.isValid() && user.getUsername().length() > 2;
+        return true;
     }
 
     public static void main(String[] args) {
