30f6962b36b5c483864b67e94ec897ef611bc2e4
Corsair <corsair954@mail.ru>
2022-06-20 17:17:17 +0300

    2. Рефакторинг - Шаблон Декоратор для валидатора. [#34117]

--- src/main/java/ru/job4j/tracker/ValidateInput.java
+++ src/main/java/ru/job4j/tracker/ValidateInput.java
@@ -1,19 +1,31 @@
 package ru.job4j.tracker;
 
-public class ValidateInput extends ConsoleInput {
+public class ValidateInput implements Input {
+    private final Input in;
+    private final Output out;
+
+    public ValidateInput(Input input, Output output) {
+        in = input;
+        out = output;
+    }
+
+    @Override
+    public String askStr(String question) {
+        return in.askStr(question);
+    }
 
     @Override
     public int askInt(String question) {
         boolean invalid = true;
-        int value = -1;
+        int rsl = -1;
         do {
             try {
-                value = super.askInt(question);
+                rsl = in.askInt(question);
                 invalid = false;
             } catch (NumberFormatException nfe) {
-                System.out.println("Please enter validate data again");
+                out.prinln("Please enter validate data again.");
             }
         } while (invalid);
-        return value;
+        return rsl;
     }
 }
