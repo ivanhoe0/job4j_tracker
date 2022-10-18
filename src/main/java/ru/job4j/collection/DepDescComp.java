fda5dbfb7058789774a892f1781b53a1fa9aa0c0
Corsair <corsair954@mail.ru>
2022-10-18 11:49:49 +0300

    2. Отсортировать департаменты [#34352]

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -5,10 +5,7 @@ import java.util.Comparator;
 public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
-        if (o1.split("/")[0].compareTo(o2.split("/")[0]) == 0) {
-            return o1.compareTo(o2);
-        } else {
-            return o2.compareTo(o1);
-        }
+        int rsl = o1.split("/")[0].compareTo(o2.split("/")[0]);
+        return rsl == 0 ? o1.compareTo(o2) : o2.compareTo(o1);
     }
 }
