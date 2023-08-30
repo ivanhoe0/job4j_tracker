bca5e9477911ac6e36e10271b061c0063007224c
Corsair <corsair954@mail.ru>
2023-08-30 18:10:25 +0300

    1. Stream API [#58166]

---
+++ src/main/java/ru/job4j/stream/FilterNegativeNumbers.java
@@ -0,0 +1,15 @@
+package ru.job4j.stream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FilterNegativeNumbers {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(12, -123, 25, -1, -4, -50, 30, 0);
+        List<Integer> positive = numbers.stream().filter(
+                i -> i > 0
+        ).collect(Collectors.toList());
+        positive.forEach(System.out::println);
+    }
+}
