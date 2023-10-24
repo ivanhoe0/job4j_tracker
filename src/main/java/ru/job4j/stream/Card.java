f78088302ae5aa0971df62feff03f84ffec04408
Corsair <corsair954@mail.ru>
2023-10-24 14:53:32 +0300

    9. Генерирование колоды карт. FlatMap [#504873]

---
+++ src/main/java/ru/job4j/stream/Card.java
@@ -0,0 +1,23 @@
+package ru.job4j.stream;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Card {
+    private Suit suit;
+    private Value value;
+
+    public Card(Suit suit, Value value) {
+        this.suit = suit;
+        this.value = value;
+    }
+
+    public static void main(String[] args) {
+        List<Card> deckOfCards = Stream.of(Suit.values())
+                .flatMap(suit -> Stream.of(Value.values())
+                        .map(value -> new Card(suit, value)))
+                        .toList();
+
+    }
+}
