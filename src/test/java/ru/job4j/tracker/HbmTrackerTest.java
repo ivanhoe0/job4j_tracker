51ee3a99467b939b7b5e678fbde1fc9391700c19
corsair <corsair954@mail.ru>
2026-03-11 16:48:25 +0300

    1. Интеграционное тестирование с Hibernate [#6875]

---
+++ src/test/java/ru/job4j/tracker/HbmTrackerTest.java
@@ -0,0 +1,18 @@
+package ru.job4j.tracker;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HbmTrackerTest {
+    @Test
+    public void whenAddNewItemThenTrackerHasSameItem() throws Exception {
+        try (var tracker = new HbmTracker()) {
+            Item item = new Item();
+            item.setName("test1");
+            tracker.add(item);
+            Item result = tracker.findById(item.getId());
+            assertThat(result.getName()).isEqualTo(item.getName());
+        }
+    }
+}
